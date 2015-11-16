ed7ce8ea08ba6f6d36a6bbdbedcd628344f4b4b0
Mike North <michael.l.north@gmail.com>
2015-11-16 22:47:58 +0800

    CD to github pages

---
+++ lib/try-generate-pages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Attempting to generate pages"
+if [[ "${TRAVIS_PULL_REQUEST}" = "false" && "${TRAVIS_BRANCH}" == 'master' && "${EMBER_TRY_SCENARIO}" == 'ember-1.13' ]]; then
+  ember github-pages:commit --message "New release" --branch "gh-pages"
+  git push deploy gh-pages 2>&1 >/dev/null
+else
+  echo "Not a main build -- no deploy"
+fi
