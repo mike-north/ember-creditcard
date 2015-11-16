5ef4644fbcbf2e7f281f8f355066d4b4c46d7616
Mike North <michael.l.north@gmail.com>
2015-11-16 22:58:59 +0800

    fix demo page

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/ember-creditcard',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
