f1a8f75f083979ec628c4f471ca467992298a3ce
Mike North <michael.l.north@gmail.com>
2016-03-09 20:00:44 +0800

    rename ember-try scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,13 +2,7 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      bower: {
-        dependencies: { }
-      }
-    },
-    {
-      name: 'ember-1-13',
+      name: 'ember-1.13',
       bower: {
         dependencies: {
           'ember': '~1.13.0'
