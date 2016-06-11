b6f5001f0254bee43b87072e72b24b34f9941384
Mike North <michael.l.north@gmail.com>
2016-06-11 16:26:56 +0800

    Add some more tests, update some dependencies

--- tests/unit/utils/luhn-test.js
+++ tests/unit/utils/luhn-test.js
@@ -4,6 +4,11 @@ import { module, test } from 'qunit';
 module('Unit | Utility | luhn');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  assert.ok(luhn('2222222'));
+test('it detects good numbers successfully', function(assert) {
+  assert.ok(luhn('4532984447715255'), 'Fake Visa Ok');
+  assert.ok(luhn('346226689213217'), 'Fake Amex Ok');
+});
+
+test('it detects bad numbers successfully', function(assert) {
+  assert.notOk(luhn('5400134368038455'), 'Bad fake MC number is not ok');
 });
--- tests/unit/utils/number-to-type-test.js
+++ tests/unit/utils/number-to-type-test.js
@@ -1,9 +1,11 @@
-// import numberToType from '../../../utils/number-to-type';
-// import { module, test } from 'qunit';
+import numberToType from '../../../utils/number-to-type';
+import { module, test } from 'qunit';
 
-// module('Unit | Utility | number to type');
+module('Unit | Utility | number to type');
 
-// // Replace this with your real tests.
-// test('it works', function(assert) {
-//   assert.equal(numberToType('4' , { allowedTypes: 'visa' }), 'visa', 'Visa detected');
-// });
+// Replace this with your real tests.
+test('it works', function(assert) {
+  assert.equal(numberToType(['4'] , { allowedTypes: 'visa|mastercard' }), 'visa', 'Visa detected');
+  assert.equal(numberToType(['343'] , { allowedTypes: 'visa|amex|discover' }), 'amex', 'Amex detected');
+  assert.equal(numberToType(['3'] , { allowedTypes: 'visa|amex|discover' }).length, 0, 'Unknown type handled');
+});
